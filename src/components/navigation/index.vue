3f42798633cf9d3ca23924cd7f6be9844495c2e2
Mesko <mateus.mesko@cplug.com.br>
2023-09-11 23:32:01 -0300

    mutation

--- src/components/navigation/index.vue
+++ src/components/navigation/index.vue
@@ -1,9 +1,17 @@
 <template>
     <div>
-        <v-btn @click="navForCreate"> criar</v-btn>
+        <v-btn @click="navForCreate()"> criar</v-btn>
         <v-btn @click="navForList"> listar</v-btn>
         <v-btn @click="navForHome"> casa</v-btn>
+
+        <v-btn @click="navAction"> action</v-btn>
+        <v-btn @click="navGetter"> getter</v-btn>
+
+        <div v-if="$store.getters.testeGetters == 1">valido</div>
+        <v-btn @click="navMutation"> mutation</v-btn>
         {{ $t('greeting') }}
+        {{ $t('HomeArea') }}
+        {{ $store.state.request }}
     </div>
 </template>
   
@@ -22,9 +30,15 @@ export default {
 
       this.$route.name != 'list' ? this.$router.push({ name: 'list' }) :  ''
     },
-    navForCreate() {xx
-      this.$route.name != 'create' ? this.$router.push({ name: 'create' }) :  ''
+    navForCreate() {
+      this.$store.commit('testeAction')
     },
+    navAction(){
+      this.$store.dispatch('testeActions')
+    },
+    navMutation(){
+      this.$store.commit('testeMutation')
+    }
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    request:[
+      {title:'teste',number:1},
+      {title:'teste2',number:2}
+    ]
   },
   getters: {
+    testeGetters(){
+      var teste = 1
+      
+      return teste
+    }
   },
   mutations: {
+    testeMutation(state){
+      console.log('teste mutation')
+    }
   },
   actions: {
+    testeActions(){
+      console.log('testeAction')
+    }
   },
   modules: {
+
   }
 })
